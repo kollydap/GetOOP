988bd1c1175bc245c53eb38904bec8e034875203
kollydap <dapkolly@gmail.com>
2022-12-22 11:46:36 +0100

    first commit

---
+++ BetterOOP/IPainter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BetterOOP
+{
+    public interface IPainter
+    {
+        bool IsAvailable { get; }
+        void EstimateTimeToPaint(double sqMeters);
+        double EstimateCompensation(double sqMeters);
+    }
+}
---
+++ BetterOOP/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BetterOOP
+{
+    class Program
+    {
+        private static IPainter FindCheapestPainter(double sqMeters, IEnumerable<IPainter> painters)
+        {
+            double bestPrice = 0;
+            IPainter cheapest = null;
+
+            foreach(IPainter painter in painters)
+            {
+                if (painter.IsAvailable)
+                {
+                    double price = painter.EstimateCompensation(sqMeters);
+                    if (cheapest == null || price < bestPrice)
+                    {
+                        cheapest = painter;
+                    }
+                }
+            }
+            return cheapest;
+        }
+
+        private static IPainter FindCheapestPainter2(double sqMeters, IEnumerable<IPainter> painters)
+        {
+            return
+                painters
+                //returns a collection of Available painters
+                    .Where(painter => painter.IsAvailable)
+                    //then aranges them based on price 
+                    .OrderBy(painter => painter.EstimateCompensation(sqMeters))
+                   //returns the first which is the cheapest
+                    .FirstOrDefault();
+
+        }
+
+        private static IPainter FindCheapestPainter3(double sqMeters, IEnumerable<IPainter> painters)
+        {
+            return
+                painters
+                    .Where(painter => painter.IsAvailable)
+                    .OrderBy(painter => painter.EstimateCompensation(sqMeters))
+                    .FirstOrDefault();
+
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
